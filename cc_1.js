e850346d9a2b9fba556b262403781a45a6bc88ca
Fernando <mhfernando778@gmail.com>
2025-01-25 19:42:04 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,15 @@ console.log(employeeID); // logs the employee ID
 console.log(typeof employeeID); // logs the type: number
 console.log(isActive); //logs the active status
 console.log(typeof isActive); // logs the type: boolean
+
+// Task 2: Product Details //
+let productName = "Game";
+const productPrice = 50;
+var isAvailable = true;
+
+console.log(productName); // logs the the product name
+console.log(typeof productName); //logs the type: string
+console.log(productPrice); // logs the price of the product
+console.log(typeof productPrice); // logs the type: number
+console.log(isAvailable); // logs the availability status
+console.log(typeof isAvailable); // logs the type: boolean
