8872a696b0ca2bda4ecb1d08e497a6d31bd37a60
Fernando <mhfernando778@gmail.com>
2025-01-25 19:35:09 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Employee Details //
+let employeeName = "Person Name";
+const employeeID = 12345;
+var isActive = true;
+
+console.log(employeeName); // logs the employee name to console
+console.log(typeof employeeName); // logs the type: string
+console.log(employeeID); // logs the employee ID
+console.log(typeof employeeID); // logs the type: number
+console.log(isActive); //logs the active status
+console.log(typeof isActive); // logs the type: boolean
